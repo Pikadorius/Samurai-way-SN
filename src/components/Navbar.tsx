fa999e161ed68e40af0ea0b9d10939e311e9e500
Pikadorius <karpukea@yandex.by>
2022-10-17 16:06:41 +0300

    experiments with 2 classes on 1 element

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import s from './Navbar.module.css';
 
+console.log(s)
+
 const Navbar = () => {
     return (
         <nav className={s.sidebar}>
-            <div className={s.item}><a>Profile</a></div>
+            <div className={`${s.item} ${s.underlined}`}><a>Profile</a></div>
             <div className={s.item}><a>Messages</a></div>
             <div className={s.item}><a>News</a></div>
             <div className={s.item}><a>Music</a></div>

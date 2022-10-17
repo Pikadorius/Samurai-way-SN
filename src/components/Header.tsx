92d28d08d6ea3d67985a998a1670cf29e2fe86bf
Pikadorius <karpukea@yandex.by>
2022-10-17 15:45:14 +0300

    add module.css import for each component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img width={'200px'} src="https://www.logodesign.net/logo/line-art-house-roof-and-buildings-4485ld.png" alt="" />
         </header>
     )
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
                     alt=""/>
+                <div>Main content</div>
             </div>
             <div className="about">
                 <img src="https://world.edu/wp-content/uploads/2020/05/Professional-Programmer.jpg" alt=""/>
-                About me
+                <div>About me</div>
             </div>
             <div className="posts">
                 Posts
-                <div>
+                <div className={s.item}>
                     New post
                 </div>
-                <div>
+                <div className={s.item}>
                     Post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     Post 2
                 </div>
             </div>
-            Main content
         </div>
     );
 };

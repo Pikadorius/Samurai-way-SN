f1a6e230ab446b7003d54cd671a5d0e939bb0842
Pikadorius <karpukea@yandex.by>
2022-10-17 14:37:34 +0300

    new Header.tsx component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <nav>
-            <span><a href="#">Home</a></span>-
-            <span><a href="#">News Feed</a></span>-
-            <span><a href="#">Messages</a></span>
-        </nav>
-    );
-};
+        <header className="header">
+            <img width={'200px'} src="https://www.logodesign.net/logo/line-art-house-roof-and-buildings-4485ld.png" alt="" />
+        </header>
+    )
+}
 
 export default Header;

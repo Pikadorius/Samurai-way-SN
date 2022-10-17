b8211f18c7016b387115eb19def4bb278d6bcc69
Pikadorius <karpukea@yandex.by>
2022-10-17 14:39:18 +0300

    added components to main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import Accordion from './components/Accordion';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
-  return (
-    <div className="App">
-        <Header/>
-        <Accordion list={['HTML', 'CSS', 'React', 'Redux']}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>
+    );
 }
 
 export default App;

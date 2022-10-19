78b90fd537991cc054693a8447cb57b9effa0cfe
Pikadorius <karpukea@yandex.by>
2022-10-19 11:26:26 +0300

    lesson-18 dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
@@ -16,7 +16,6 @@ const Profile = () => {
                      alt=""/>
                 <div>About me</div>
             </div>
-
             <MyPosts/>
 
         </div>

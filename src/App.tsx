77f860176df231461afc8ad91bf335cf4e7d91e1
Pikadorius <karpukea@yandex.by>
2022-10-19 18:49:48 +0300

    lesson-19 route + new components

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
     return (
-        <div className='App'>
-            <Header list={['Video', 'Music', 'Photos']}/>
-            <Navbar/>
-
-            <div className='app-wrapper-content'>
-                {/*<Profile/>*/}
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='App'>
+                <Header list={['Video', 'Music', 'Photos']}/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                        <Route component={Profile} path={'/profile'}/>
+                        <Route component={Dialogs} path={'/dialogs'}/>
+                        <Route component={News} path={'/news'}/>
+                        <Route component={Music} path={'/music'}/>
+                        <Route component={Settings} path={'/settings'}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 

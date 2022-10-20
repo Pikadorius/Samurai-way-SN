808f51c61845430a160f4c657c4acbb544a5101e
Pikadorius <karpukea@yandex.by>
2022-10-20 15:16:50 +0300

    change css rules profile img

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
         <div>
             <div>
-                <img
+                <img className={s.pageImg}
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
                     alt=""/>
                 <div>Main content</div>

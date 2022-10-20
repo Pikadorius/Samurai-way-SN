16ec47dbf536b35db65d2005d421a62a092b77ab
Pikadorius <karpukea@yandex.by>
2022-10-20 15:17:45 +0300

    change a => NavLink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 console.log(s)
 
 const Navbar = () => {
     return (
         <nav className={s.sidebar}>
-            <div className={`${s.item} ${s.active}`}><a href={'/profile'}>Profile</a></div>
-            <div className={s.item}><a href={'/dialogs'}>Dialogs</a></div>
-            <div className={s.item}><a href={'/news'}>News</a></div>
-            <div className={s.item}><a href={'/music'}>Music</a></div>
-            <div className={s.item}><a href={'/settings'}>Settings</a></div>
+            <div className={s.item}>
+                <NavLink to={'/profile'} activeClassName={s.activeLink}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/dialogs'} activeClassName={s.activeLink}>Dialogs</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/news'} activeClassName={s.activeLink}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/music'} activeClassName={s.activeLink}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/settings'} activeClassName={s.activeLink}>Settings</NavLink>
+            </div>
         </nav>
     );
 };

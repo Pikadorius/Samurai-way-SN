b415093ea87f3025c9b8c32d0ccb904e5f484a1a
Pikadorius <karpukea@yandex.by>
2022-10-26 17:24:33 +0300

    change component to render. added type for App. added required properties for Dialogs component

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,26 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+import Dialogs, {DialogItemType, MessageType} from './components/Dialogs/Dialogs';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Music from './components/Music/Music';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 
-const App = () => {
+type AppType = {
+    dialogsData: DialogItemType[]
+    messagesData: MessageType[]
+}
+
+const App:React.FC<AppType> = ({dialogsData,messagesData}) => {
     return (
         <BrowserRouter>
             <div className='App'>
                 <Header list={['Video', 'Music', 'Photos']}/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route component={Profile} path={'/profile'}/>
-                    <Route component={Dialogs} path={'/dialogs'}/>
+                    <Route render={Profile} path={'/profile'}/>
+                    <Route render={()=><Dialogs dialogsData={dialogsData} messagesData={messagesData}/>} path={'/dialogs'}/>
                     <Route component={News} path={'/news'}/>
                     <Route component={Music} path={'/music'}/>
                     <Route component={Settings} path={'/settings'}/>

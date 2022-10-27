a66f4aa5999e618d4a73f54841bad50ac2bce37f
Pikadorius <karpukea@yandex.by>
2022-10-27 14:36:26 +0300

    refactor due to new components

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs, {DialogItemType, MessageType} from './components/Dialogs/Dialogs';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Music from './components/Music/Music';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
+import Dialogs from './components/Dialogs/Dialogs';
+import {DialogItemType} from './components/Dialogs/DialogItem/DialogItem';
+import {MessageType} from './components/Dialogs/Message/Message';
 
 type AppType = {
     dialogsData: DialogItemType[]

c306f960a41167b36b1a68ceceb1e39ed1e6191d
Pikadorius <karpukea@yandex.by>
2022-10-27 13:23:13 +0300

    added new variable for mapped elements

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,12 +5,16 @@ import Post, {PostType} from './Post/Post';
 
 const postsData:PostType[]= [
     {id:1,name:"My  first post", description: "I try to set props to my firts post...", likesCount: 0},
-    {id:2,name:"It works, I'm very excited!", description: "Hmmm... I really enjoy the result!", likesCount: 0}
+    {id:2,name:"It works, I'm very excited!", description: "Hmmm... I really enjoy the result!", likesCount: 0},
+    {id:3,name:"Dimych is the best!", description: "Dimych has a talant to teach", likesCount: 10}
 ]
 
 
 
 const MyPosts = () => {
+
+    const allPosts=postsData.map(p=><Post name={p.name} description={p.description} likesCount={p.likesCount} id={p.id}/>)
+
     return (
         <div className={s.content}>
 
@@ -21,8 +25,9 @@ const MyPosts = () => {
                     <button>Add post</button>
                     <button>Delete post</button>
                 </div>
-                <Post name={postsData[0].name} description={postsData[0].description} likesCount={postsData[0].likesCount} id={postsData[0].id}/>
-                <Post name={postsData[1].name} description={postsData[1].description} likesCount={postsData[1].likesCount} id={postsData[1].id}/>
+                <div>
+                    {allPosts}
+                </div>
             </div>
         </div>
     );

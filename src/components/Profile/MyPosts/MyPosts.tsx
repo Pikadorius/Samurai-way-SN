737e800f3b897e4deee1947142dc67d2c5f37fe2
Pikadorius <karpukea@yandex.by>
2022-11-02 12:46:57 +0300

    added ref on textarea

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {LegacyRef} from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import {PostsType} from '../../../redux/state';
 
-const MyPosts: React.FC<PostsType>= ({posts}) => {
+const MyPosts: React.FC<PostsType> = ({posts}) => {
 
-    const allPosts=posts.map(p=><Post name={p.title} description={p.description} likesCount={p.likesCount} id={p.id}/>)
+    const allPosts = posts.map(p => <Post name={p.title}
+                                          description={p.description}
+                                          likesCount={p.likesCount}
+                                          id={p.id}/>)
+
+    const newPostElement: LegacyRef<HTMLTextAreaElement> = React.createRef();
+
+
+    const addPost = () => {
+        debugger
+        let text = newPostElement.current?.value;
+        alert(text)
+    }
 
     return (
         <div className={s.content}>
             <div className={s.postsBlock}>
                 <h3>Posts</h3>
                 <div>
-                    <textarea placeholder={'Write something... '} cols={50} rows={10}></textarea>
-                    <button>Add post</button>
-                    <button>Delete post</button>
+                    <textarea placeholder={'Write something... '} cols={30} rows={5} ref={newPostElement}></textarea>
+                    <button onClick={addPost}>Add post</button>
                 </div>
                 <div>
                     {allPosts}

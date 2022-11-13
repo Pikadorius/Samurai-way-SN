876e1f22217e8d4fb942a2d81c300633a42fbe09
Pikadorius <karpukea@yandex.by>
2022-11-13 16:19:16 +0300

    some changes

--- src/render.tsx
+++ src/render.tsx
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import {addLikeForPost, addNewMessage, addNewPost, setNewMessageText, setNewPostText, StateType} from './redux/state';
 
-export const rerenderEntireTree = (state: StateType) => {
+export const rerenderEntireTree = (stateForSN: StateType) => {
     ReactDOM.render(
-        <App state={state} addNewPost={addNewPost} addNewMessage={addNewMessage} setNewPostText={setNewPostText}
+        <App state={stateForSN} addNewPost={addNewPost} addNewMessage={addNewMessage} setNewPostText={setNewPostText}
              setNewMessageText={setNewMessageText} addLikeForPost={addLikeForPost}/>,
         document.getElementById('root')
     );

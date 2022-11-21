f579e2d293d441804080600f55ee9388e7f63ab6
Pikadorius <karpukea@yandex.by>
2022-11-21 13:44:01 +0300

    realised logic in reducers

--- src/redux/dialogs_reducer.ts
+++ src/redux/dialogs_reducer.ts
@@ -1,5 +1,21 @@
-import {ActionsType, StateType} from "./store";
+import {ActionsType, DialogsPageType, MessageType} from "./store";
 
-const dialogsReducer = (state:StateType, action: ActionsType):StateType => {
+const dialogsReducer = (state: DialogsPageType, action: ActionsType): DialogsPageType => {
+
+    const ADD_MESSAGE = 'ADD-NEW-MESSAGE'
+    const SET_MESSAGE = 'SET-MESSAGE-TEXT'
+
+    if (action.type === ADD_MESSAGE) {
+        let newMessage: MessageType = {
+            id: state.messages.length + 1,
+            message: state.newMessageText
+        }
+        state.messages.push(newMessage);
+        state.newMessageText = '';
+    } else if (action.type === SET_MESSAGE) {
+        state.newMessageText = action.newMessageText;
+    }
+
+
     return state;
 }
--- src/redux/profile_reducer.ts
+++ src/redux/profile_reducer.ts
@@ -1,5 +1,26 @@
-import {ActionsType, StateType} from "./store";
+import {ActionsType, PostType, ProfilePageType} from "./store";
 
-const profileReducer = (state:StateType, action: ActionsType):StateType => {
+const profileReducer = (state: ProfilePageType, action: ActionsType): ProfilePageType => {
+
+    const ADD_POST = 'ADD-NEW-POST'
+    const SET_POST_TEXT = 'SET-POST-TEXT'
+    const ADD_LIKE = 'ADD-LIKE'
+
+    if (action.type === ADD_POST) {
+        let newPost: PostType = {
+            id: state.posts.length + 1,
+            title: `Post ${state.posts.length + 1}`,
+            description: state.newPostText,
+            likesCount: 0
+        };
+        state.posts.push(newPost);
+        state.newPostText = "";
+    } else if (action.type === SET_POST_TEXT) {
+        state.newPostText = action.newPostText;
+
+    } else if (action.type === ADD_LIKE) {
+        state.posts.map(p => p.id === action.postId ? p.likesCount++ : p);
+    }
+
     return state;
 }

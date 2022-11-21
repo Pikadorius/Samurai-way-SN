9e644187d7285afafa0ea4d48537773b7124f4cd
Pikadorius <karpukea@yandex.by>
2022-11-21 14:34:47 +0300

    change if-else on switch, added actionCreators

--- src/redux/dialogs_reducer.ts
+++ src/redux/dialogs_reducer.ts
@@ -1,23 +1,36 @@
 import {ActionsType, DialogsPageType, MessageType} from "./store";
 
+const ADD_MESSAGE = 'ADD-NEW-MESSAGE'
+const SET_MESSAGE = 'SET-MESSAGE-TEXT'
+
 const dialogsReducer = (state: DialogsPageType, action: ActionsType): DialogsPageType => {
 
-    const ADD_MESSAGE = 'ADD-NEW-MESSAGE'
-    const SET_MESSAGE = 'SET-MESSAGE-TEXT'
-
-    if (action.type === ADD_MESSAGE) {
-        let newMessage: MessageType = {
-            id: state.messages.length + 1,
-            message: state.newMessageText
-        }
-        state.messages.push(newMessage);
-        state.newMessageText = '';
-    } else if (action.type === SET_MESSAGE) {
-        state.newMessageText = action.newMessageText;
+    switch (action.type) {
+        case ADD_MESSAGE:
+            let newMessage: MessageType = {
+                id: state.messages.length + 1,
+                message: state.newMessageText
+            }
+            state.messages.push(newMessage);
+            state.newMessageText = '';
+            return state;
+            break;
+        case SET_MESSAGE:
+            state.newMessageText = action.newMessageText;
+            return state;
+            break;
+        default:
+            return state;
     }
-
-
-    return state;
 }
+
+export const addMessageActionCreator = () => {
+    return {type: ADD_MESSAGE} as const
+}
+
+export const setMessageActionCreator = (newMessageText: string) => {
+    return {type: SET_MESSAGE, newMessageText: newMessageText} as const
+}
+
 //редьюсеры эскпортируем по умолчанию
 export default dialogsReducer;
--- src/redux/profile_reducer.ts
+++ src/redux/profile_reducer.ts
@@ -1,28 +1,45 @@
 import {ActionsType, PostType, ProfilePageType} from "./store";
 
-const profileReducer = (state: ProfilePageType, action: ActionsType): ProfilePageType => {
+const ADD_POST = 'ADD-NEW-POST'
+const SET_POST_TEXT = 'SET-POST-TEXT'
+const ADD_LIKE = 'ADD-LIKE'
 
-    const ADD_POST = 'ADD-NEW-POST'
-    const SET_POST_TEXT = 'SET-POST-TEXT'
-    const ADD_LIKE = 'ADD-LIKE'
+const profileReducer = (state: ProfilePageType, action: ActionsType):ProfilePageType => {
 
-    if (action.type === ADD_POST) {
-        let newPost: PostType = {
-            id: state.posts.length + 1,
-            title: `Post ${state.posts.length + 1}`,
-            description: state.newPostText,
-            likesCount: 0
-        };
-        state.posts.push(newPost);
-        state.newPostText = "";
-    } else if (action.type === SET_POST_TEXT) {
-        state.newPostText = action.newPostText;
-
-    } else if (action.type === ADD_LIKE) {
-        state.posts.map(p => p.id === action.postId ? p.likesCount++ : p);
+    switch (action.type) {
+        case ADD_POST:
+            let newPost: PostType = {
+                id: state.posts.length + 1,
+                title: `Post ${state.posts.length + 1}`,
+                description: state.newPostText,
+                likesCount: 0
+            };
+            state.posts.push(newPost);
+            state.newPostText = "";
+            return state;
+            break;
+        case SET_POST_TEXT:
+            state.newPostText = action.newPostText;
+            return state;
+            break;
+        case ADD_LIKE:
+            state.posts.map(p => p.id === action.postId ? p.likesCount++ : p);
+            return state;
+            break;
+        default:
+            return state;
     }
+}
 
-    return state;
+export const addPostActionCreator = () => {
+    return {type: ADD_POST} as const
+}
+
+export const setPostActionCreator = (newPostText: string) => {
+    return {type: SET_POST_TEXT, newPostText: newPostText} as const
+}
+export const addLIkeActionCreator = (postId: number) => {
+    return {type: ADD_LIKE, postId} as const
 }
 
 //редьюсеры эскпортируем по умолчанию

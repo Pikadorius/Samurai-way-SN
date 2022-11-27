6e62363349f6aaaca93f82c522f56509b4c10cfc
Pikadorius <karpukea@yandex.by>
2022-11-27 12:21:33 +0300

    added MyPostsContainer.tsx (lesson 43 samurai)

---
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,46 @@
+import React, {ChangeEvent} from 'react';
+import s from './MyPosts.module.css';
+import Post from './Post/Post';
+import {ActionsType, PostType} from '../../../redux/store';
+import {addPostActionCreator, setPostActionCreator} from "../../../redux/profile_reducer";
+
+type MyPostsType = {
+    newPostText: string
+    posts: PostType[]
+    dispatch: (action: ActionsType)=>void
+}
+
+const MyPostsContainer: React.FC<MyPostsType> = ({newPostText,posts,dispatch}) => {
+
+    const allPosts = posts.map(p => <Post name={p.title}
+                                          description={p.description}
+                                          likesCount={p.likesCount}
+                                          id={p.id}
+                                          key={p.id}
+                                          dispatch={dispatch}/>)
+
+    const setPost = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        dispatch(setPostActionCreator(e.currentTarget.value))
+    }
+
+    const addNewPostCallback = () => {
+        dispatch(addPostActionCreator())
+    }
+
+    return (
+        <div className={s.content}>
+            <div className={s.postsBlock}>
+                <h3>Posts</h3>
+                <div>
+                    <textarea cols={30} rows={5} value={newPostText} onChange={setPost}></textarea>
+                    <button onClick={addNewPostCallback}>Add post</button>
+                </div>
+                <div>
+                    {allPosts}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MyPostsContainer;

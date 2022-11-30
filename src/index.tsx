bd1cdb7429f0f2ca055385de4f61c699f5c25143
Pikadorius <karpukea@yandex.by>
2022-11-30 22:41:19 +0300

    deleted rerender

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,20 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from "react-redux";
 
 
+// added Provider, so we don't need to rerender all if something changes
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App store={store}/>
+        </Provider>
+    </BrowserRouter>, document.getElementById('root'))
+
+
+/*
+
+// added Provider, so we don't need to rerender all if something changes
+
 // function that render JSX
 const rerenderEntireTree = () => {
     ReactDOM.render(
@@ -18,9 +32,12 @@ const rerenderEntireTree = () => {
     );
 }
 
+
+
 // subscribe to store (if state changes - will be called)
 store.subscribe(rerenderEntireTree)
 
 // first render
 rerenderEntireTree()
 
+*/

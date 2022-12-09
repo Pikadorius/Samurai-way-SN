588bb53bcc9d6a118f4b61fdd5d0a4b80b31bfa1
Pikadorius <karpukea@yandex.by>
2022-12-09 19:20:52 +0300

    rewrited with hooks (useSelector and useDispatch)

--- src/components/Users/UsersFunctional.tsx
+++ src/components/Users/UsersFunctional.tsx
@@ -3,23 +3,24 @@ import s from './Users.module.css'
 import {UsersPropsType} from "./UsersContainer";
 import axios from 'axios';
 import avatar from '../../assets/images/defaultUsersAvatar.jpg'
+import {useDispatch, useSelector} from 'react-redux';
+import {StateType} from '../../redux/redux-store';
+import {deleteUserAC, followAC, InitialStateType, setUsersAC, showMoreAC, unfollowAC} from '../../redux/users-reducer';
 
-const UsersFunctional: React.FC<UsersPropsType> = ({usersPage, follow, unfollow, showMore, setUsers, deleteUser}) => {
+const UsersFunctional: React.FC = () => {
 
-    const filteredUser = usersPage.users.filter((u, i) => i < usersPage.count)
+    const state=useSelector<StateType, InitialStateType>(state=>state.usersPage)
+    const dispatch=useDispatch()
+
+    const filteredUser = state.users.filter((u, i) => i < state.count)
 
     const getUsers = () => {
-
         debugger
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
             debugger
-            usersPage.users.length ? alert('No more new users') : setUsers(response.data.items)
+            state.users.length ? alert('No more new users') : dispatch(setUsersAC(response.data.items))
         })
 
-    }
-
-    const showMoreHandler = () => {
-        showMore()
     }
 
 
@@ -30,7 +31,7 @@ const UsersFunctional: React.FC<UsersPropsType> = ({usersPage, follow, unfollow,
                 {filteredUser.map(u => {
 
                     const followHandler = () => {
-                        u.followed ? unfollow(u.id) : follow(u.id)
+                        u.followed ? dispatch(unfollowAC(u.id)) : dispatch(followAC(u.id))
                     }
 
                     const userClassName = u.followed ? `${s.userItem} ${s.followed}` : s.userItem
@@ -41,7 +42,7 @@ const UsersFunctional: React.FC<UsersPropsType> = ({usersPage, follow, unfollow,
                                                                alt="avatar"/></div>
                             <div>{u.name}</div>
                             <div className={s.location}></div>
-                            <button className={s.btn} onClick={() => deleteUser(u.id)}>x</button>
+                            <button className={s.btn} onClick={() => dispatch(deleteUserAC(u.id))}>x</button>
                         </div>
                         <div className={s.statusBar}>{u.status}</div>
                         <button onClick={followHandler}>{u.followed ? 'Unfollow' : 'Follow'}</button>
@@ -49,7 +50,7 @@ const UsersFunctional: React.FC<UsersPropsType> = ({usersPage, follow, unfollow,
                 })}
             </div>
             <div>
-                <button onClick={showMoreHandler} disabled={usersPage.users.length <= usersPage.count}>Show more
+                <button onClick={()=>dispatch(showMoreAC())} disabled={state.users.length <= state.count}>Show more
                 </button>
                 <button onClick={getUsers}>Get new users</button>
             </div>

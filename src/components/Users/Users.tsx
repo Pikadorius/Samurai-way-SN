ea85f79b88108ef209340fe05b58826cc115be29
Pikadorius <karpukea@yandex.by>
2022-12-13 14:27:34 +0300

    lesson 56 flow 2 container components (all working)

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import s from "./Users.module.css";
+import avatar from "../../assets/images/defaultUsersAvatar.jpg";
+import {InitialStateType} from "../../redux/users-reducer";
+
+type UsersType = {
+    usersPage: InitialStateType
+    onPageChanged:(n:number)=>void
+    follow: (id: number) => void
+    unfollow: (id: number) => void
+    deleteUser: (id: number)=>void
+    setCurrentPage: (pageNumber:number)=>void
+}
+
+const Users = (props: UsersType) => {
+
+    // destructuring usersPage
+    const {users, totalUsersCount, currentPage, pageSize} = props.usersPage
+    // calculate pages count
+    let pagesCount: number = Math.ceil(totalUsersCount / pageSize)
+    // create pages array
+    let pages: number[] = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+    }
+
+    let curPF = ((currentPage - 5) < 0) ? 0 : currentPage - 5;
+    let curPL = currentPage + 5;
+    let slicedPages = pages.slice(curPF, curPL);
+
+    return (
+        <div>
+            {/*pagination*/}
+            <div className={s.pagination}>
+                {
+                    slicedPages.map(p => <span onClick={() => props.onPageChanged(p)} key={p}
+                                               className={currentPage === p ? s.selectedPage : ""}> {p} </span>)
+                } <input placeholder={`1-${pagesCount}`} onChange={(e) => props.onPageChanged(+e.currentTarget.value)}/>
+            </div>
+
+            <div className={s.usersField}>
+
+                {users.map(u => {
+
+                    const followHandler = () => {
+                        u.followed ? props.unfollow(u.id) : props.follow(u.id)
+                    }
+
+
+                    const userClassName = u.followed ? `${s.userItem} ${s.followed}` : s.userItem
+
+                    return <div className={userClassName} key={u.id}>
+                        <div className={s.about}>
+                            <div className={s.userAvatar}><img src={u.photos.small ? u.photos.small : avatar}
+                                                               alt="avatar"/></div>
+                            <div>{u.name}</div>
+                            <button className={s.btn} onClick={() => props.deleteUser(u.id)}>x</button>
+                        </div>
+                        <div className={s.statusBar}>{u.status}</div>
+                        <button onClick={followHandler}>{u.followed ? 'Unfollow' : 'Follow'}</button>
+                    </div>
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Users;
--- src/components/Users/UsersAPIComponent.tsx
+++ src/components/Users/UsersAPIComponent.tsx
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-import s from './Users.module.css';
-import avatar from '../../assets/images/defaultUsersAvatar.jpg';
 import axios from 'axios';
 import {UsersPropsType} from './UsersContainer';
+import Users from "./Users";
 
 class UsersAPIComponent extends Component<UsersPropsType> {
 
@@ -24,7 +23,7 @@ class UsersAPIComponent extends Component<UsersPropsType> {
         console.log('Component Users die...')
     }
 
-    updateUsers = (p:number) => {
+    onPageChanged = (p:number) => {
         this.props.setCurrentPage(p)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${p}&count={${this.props.usersPage.pageSize}`).then((response) => {
             this.props.setUsers(response.data.items)
@@ -33,54 +32,12 @@ class UsersAPIComponent extends Component<UsersPropsType> {
 
     render = () => {
         console.log('Users rendering')
-
-        // destructuring usersPage
-        const {users,totalUsersCount,currentPage,pageSize}=this.props.usersPage
-        // calculate pages count
-        let pagesCount: number = Math.ceil(totalUsersCount / pageSize)
-        // create pages array
-        let pages: number[] = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-
-        let curPF = ((currentPage - 5) < 0) ?  0  : currentPage - 5 ;
-        let curPL = currentPage + 5;
-        let slicedPages = pages.slice(curPF, curPL);
-        return (
-            <div>
-                {/*pagination*/}
-                <div className={s.pagination}>
-                    {
-                        slicedPages.map(p => <span onClick={()=>this.updateUsers(p)} key={p} className={currentPage===p ? s.selectedPage : ""}> {p} </span>)
-                    } <input placeholder={`1-${pagesCount}`} onChange={(e)=>this.updateUsers(+e.currentTarget.value)}/>
-                </div>
-
-                <div className={s.usersField}>
-
-                    {users.map(u => {
-
-                        const followHandler = () => {
-                            u.followed ? this.props.unfollow(u.id) : this.props.follow(u.id)
-                        }
-
-
-                        const userClassName = u.followed ? `${s.userItem} ${s.followed}` : s.userItem
-
-                        return <div className={userClassName} key={u.id}>
-                            <div className={s.about}>
-                                <div className={s.userAvatar}><img src={u.photos.small ? u.photos.small : avatar}
-                                                                   alt="avatar"/></div>
-                                <div>{u.name}</div>
-                                <button className={s.btn} onClick={() => this.props.deleteUser(u.id)}>x</button>
-                            </div>
-                            <div className={s.statusBar}>{u.status}</div>
-                            <button onClick={followHandler}>{u.followed ? 'Unfollow' : 'Follow'}</button>
-                        </div>
-                    })}
-                </div>
-            </div>
-        );
+        return <Users usersPage={this.props.usersPage}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+                      deleteUser={this.props.deleteUser}
+                      setCurrentPage={this.props.setCurrentPage}
+                      onPageChanged={this.onPageChanged}/>
     }
 }
 

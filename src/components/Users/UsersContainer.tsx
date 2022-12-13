fbe5ef4127c40cec2b0e8cacdbc40a36144a0587
Pikadorius <karpukea@yandex.by>
2022-12-13 14:32:03 +0300

    removed API container class component to connect (container) component

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {StateType} from "../../redux/redux-store";
 import {
@@ -10,14 +10,14 @@ import {
     UserType
 } from "../../redux/users-reducer";
 import {Dispatch} from "redux";
-import UsersAPIComponent from './UsersAPIComponent';
 import UsersFunctional from "./UsersFunctional";
+import axios from "axios";
+import Users from "./Users";
 
 
 type MapStateType = {
     usersPage: InitialStateType
 }
-
 const mapStateToProps = (state: StateType): MapStateType => {
     return {
         usersPage: state.usersPage
@@ -32,7 +32,6 @@ type MapDispatchType = {
     setCurrentPage: (pageNumber:number)=>void
     setTotalUsersCount: (totalCount: number)=>void
 }
-
 const mapDispatchToProps = (dispatch: Dispatch): MapDispatchType => {
     return {
         follow: (id) => {
@@ -48,7 +47,44 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchType => {
     }
 }
 
-export type UsersPropsType = MapStateType & MapDispatchType
+type UsersPropsType = MapStateType & MapDispatchType
+class UsersAPIComponent extends Component<UsersPropsType> {
+
+    componentDidMount() {
+        console.log('Users are inside DOM')
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.usersPage.currentPage}&count={${this.props.usersPage.pageSize}`).then((response) => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+            console.log(this.props.usersPage.totalUsersCount)
+        })
+        //https://social-network.samuraijs.com/api/1.0/users
+    }
+
+    componentDidUpdate(prevProps: Readonly<UsersPropsType>, prevState: Readonly<{}>, snapshot?: any) {
+        console.log('Users have been updated')
+    }
+
+    componentWillUnmount() {
+        console.log('Component Users die...')
+    }
+
+    onPageChanged = (p:number) => {
+        this.props.setCurrentPage(p)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${p}&count={${this.props.usersPage.pageSize}`).then((response) => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render = () => {
+        console.log('Users rendering')
+        return <Users usersPage={this.props.usersPage}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+                      deleteUser={this.props.deleteUser}
+                      setCurrentPage={this.props.setCurrentPage}
+                      onPageChanged={this.onPageChanged}/>
+    }
+}
 
 const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
 

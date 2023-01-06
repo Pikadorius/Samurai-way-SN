cb51e5c4dc6f39a05fd791ccaedce6386fcc390d
Pikadorius <karpukea@yandex.by>
2023-01-06 20:36:37 +0300

    added new AC and ThunkCreators to authReducer

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,9 +1,9 @@
 import {Dispatch} from 'redux';
-import {authAPI} from '../API/API';
+import {authAPI, LoginFormType} from '../API/API';
 
 const enum ACTIONS_TYPE {
     SET_USER_DATA = 'SET_USER_DATA',
-    SET_USER_PHOTO = 'SET_USER_PHOTO'
+    LOG_OUT = 'LOG_OUT'
 }
 
 export type InititalStateType = {
@@ -23,12 +23,16 @@ const initailState: InititalStateType = {
 }
 
 type ActionType =
-    SetUserDataACType
+    SetUserDataACType |
+    LogOutUserACType
 
 export const authReducer = (state: InititalStateType = initailState, action: ActionType): InititalStateType => {
     switch (action.type) {
         case ACTIONS_TYPE.SET_USER_DATA: {
             return {...state, ...action.payload.data, isAuth: true}
+        }
+        case ACTIONS_TYPE.LOG_OUT: {
+            return initailState;
         }
         default:
             return state
@@ -45,11 +49,38 @@ export const setAuthUserData = (data: InititalStateType) => {
     } as const
 }
 
-export type AuthUserTCType = ()=>void
-export const authUser = ()=>(dispatch:Dispatch)=>{
+export type LogOutUserACType = ReturnType<typeof logOutUser>
+export const logOutUser = () => {
+    return {
+        type: ACTIONS_TYPE.LOG_OUT
+    } as const
+}
+
+export type AuthUserTCType = () => void
+export const authUser = () => (dispatch: Dispatch) => {
     authAPI.authMe().then((result) => {
         if (result.resultCode === 0) {
             dispatch(setAuthUserData(result.data))
         }
+    })
+}
+
+export type AuthFromLogin = (loginData: LoginFormType) => void
+export const authFromLogin: AuthFromLogin = (loginData) => (dispatch: Dispatch) => {
+    authAPI.login(loginData).then(res => {
+        if (res.data.resultCode === 0) {
+            authAPI.authMe().then(res => {
+                console.log('authorize me!!!!')
+                dispatch(setAuthUserData(res.data))
+            })
+        }
+        else alert('Something wrong')
+    })
+}
+
+export type LogoutTCType = () => void
+export const logoutTC = () => (dispatch: Dispatch) => {
+    authAPI.logout().then(()=>{
+        dispatch(logOutUser())
     })
 }

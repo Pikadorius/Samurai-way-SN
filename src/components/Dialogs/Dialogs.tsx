1247edb8708925aeff8afa7908c2fec61231256c
Pikadorius <karpukea@yandex.by>
2023-01-14 10:46:03 +0300

    pre-lesson 76 added form for message

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,11 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, FC} from 'react';
 import s from './Dialogs.module.css'
 import Message from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
 import {DialogsType} from "./DialogsContainer";
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
+import {useDispatch} from 'react-redux';
+import {addMessageActionCreator, setMessageActionCreator} from '../../redux/dialogs_reducer';
 
 
 const Dialogs: React.FC<DialogsType> = ({dialogsState, addMessage, setMessage, deleteMessage}) => {
@@ -16,8 +19,13 @@ const Dialogs: React.FC<DialogsType> = ({dialogsState, addMessage, setMessage, d
         addMessage()
     }
 
-    const onChangeMessageHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        setMessage(e.currentTarget.value)
+    const onChangeMessageHandler = (formData: MessageFormType) => {
+        setMessage(formData.message)
+    }
+
+    const handleSubmit = (formData:MessageFormType) => {
+        onChangeMessageHandler(formData)
+        onClickAddMessage()
     }
 
     return (
@@ -28,9 +36,10 @@ const Dialogs: React.FC<DialogsType> = ({dialogsState, addMessage, setMessage, d
             <div className={s.messages}>
                 <div>{messagesElements}</div>
                 <div>
-                    <textarea value={dialogsState.newMessageText} onChange={onChangeMessageHandler}
+                    {/*<textarea value={dialogsState.newMessageText} onChange={onChangeMessageHandler}
                               placeholder={'Enter new message...'}/>
-                    <button onClick={onClickAddMessage}>Add message</button>
+                    <button onClick={onClickAddMessage}>Add message</button>*/}
+                    <MessageFormRedux onSubmit={handleSubmit}/>
                 </div>
             </div>
         </div>
@@ -38,3 +47,40 @@ const Dialogs: React.FC<DialogsType> = ({dialogsState, addMessage, setMessage, d
 };
 
 export default Dialogs;
+
+
+
+
+
+
+type MessageFormType = {
+    message: string
+}
+
+const MessageForm:FC<InjectedFormProps<MessageFormType>> = (props)=> {
+    return <form onSubmit={props.handleSubmit}>
+        <Field component='textarea' name={'message'} placeholder={'Enter new message...'}></Field>
+        <button>Add message</button>
+    </form>
+}
+
+const MessageFormRedux = reduxForm<MessageFormType>({
+    form: 'dialogAddeMessageForm'
+})(MessageForm)
+
+
+/*
+
+const Messages = () => {
+
+    const dispatch = useDispatch()
+
+    const handleSubmit = (formData:MessageFormType) => {
+        console.log(formData)
+        dispatch(setMessageActionCreator(formData.message))
+        debugger
+        dispatch(addMessageActionCreator())
+    }
+
+    return <MessageFormRedux onSubmit={handleSubmit}/>
+}*/
